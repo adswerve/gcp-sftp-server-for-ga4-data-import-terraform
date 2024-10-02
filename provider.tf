da76d07e19d2eb7acb83b0d066e0f47989a6093a
Ryan Ptomey <rptomey6@gmail.com>
2024-10-02 15:15:12 -0500

    added project services module

--- provider.tf
+++ provider.tf
@@ -10,3 +10,12 @@ terraform {
 provider "google" {
   project = var.project_id
 }
+
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 17.0"
+
+  project_id = var.project_id
+  activate_apis = var.gcp_service_list
+  disable_services_on_destroy = false
+}
