73df376eb88a77636a53c0387427a0fc949923bc
Ryan Ptomey <rptomey6@gmail.com>
2024-10-02 15:15:53 -0500

    creating a GCS bucket for file storage

---
+++ storage.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "file_bucket" {
+  name = "${var.name}-files"
+  location = "US"
+  force_destroy = true
+
+  autoclass {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+  public_access_prevention = "enforced"
+
+  depends_on = [ module.project-services ]
+}
